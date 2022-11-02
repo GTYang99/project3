cb39e2f570e23ccacb2741fc9ca7d247693004a3
GT.Yang <gaga0099@gmail.com>
2022-11-02 14:54:46 +0800

    建立視窗、下載文字

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import tkinter as tk 
+from datetime import datetime
+from pyglet import font
+
 class Windows(tk.Tk):
     def __init__(self):
         super().__init__()
-
+        now = datetime.now()
+        now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+        font.add_file('action_man.ttf')
+        
+        tk.Label(self,text=now_str,font=("Times New Roman",30)).pack(padx=50,pady=30)
 
 
 def main():
