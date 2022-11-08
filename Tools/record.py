31a17db88bf25475a6901679c5f8e74b4b21ee90
GT.Yang <gaga0099@gmail.com>
2022-11-08 11:04:23 +0800

    完成紀錄數據，按照設定秒數寫成CSV

--- Tools/record.py
+++ Tools/record.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+import csv
 
 directory = os.path.abspath("./record")
 # Python 判斷資料夾是否存在 os.path.isdir
@@ -20,5 +21,13 @@ def recordData(distance,lightValue):
     if filename not in currentFiles:
         print(f"沒有{filename}檔案")
         # directory是相對路徑下的record資料夾，filename是檔案名
-        file = open(filename_abs,'w',encoding='utf-8')
+        # csv檔案要有newline
+        file = open(filename_abs,'w',encoding='utf-8',newline='')
+        # 表標題內容:先寫入，再確定表標題內容
+        header_writer = csv.writer(file)
+        header_writer.writerow(["日期","距離","光線"])
         file.close()
+    with open(filename_abs,"a",encoding='utf-8')as file:
+        csv_writer = csv.writer(file)
+        # 每次有新數據進來，就寫入row一次
+        csv_writer.writerow([current.strftime("%Y-%m-%d %H:%M:%S"),distance,lightValue]) 
